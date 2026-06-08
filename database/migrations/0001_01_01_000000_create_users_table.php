444cafaa908b247217a4dd1119ccc77a8fc9901b
juber <jgmathakiya21@gmail.com>
2026-06-08 11:00:22 +0530

    role_id migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('role_id')->nullable();
             $table->unsignedBigInteger('parent_id')->nullable();
             $table->string('first_name', 50)->nullable();
             $table->string('last_name', 50)->nullable();
